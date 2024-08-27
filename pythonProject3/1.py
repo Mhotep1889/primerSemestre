8d4bc9001f9e5cc05024084417c160c439a0eb2c
Mhotep1889 <felipe1889@hotmail.com>
2024-08-26 20:29:46 -0500

    Add files via upload

---
+++ pythonProject3/1.py
@@ -0,0 +1,20 @@
+import math
+a = 7
+print(a)
+
+#identacion
+x = 10
+y = 20
+if x >y:
+    print("x es mayor que y")
+else:
+    print("x es menor que y")
+
+
+
+nombre = "Felipe"
+apellido = "Gaviria"
+print(f"hola {nombre}, como estas?")
+
+print(math.pow(10, 3)) #1000.0
+
